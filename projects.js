f4b9c16c998999ece5802779a0add53e023437ba
Emmnia <eugmalikova@gmail.com>
2024-09-28 21:32:45 +0300

    4

---
+++ projects.js
@@ -0,0 +1,20 @@
+const swiper = new Swiper('.mySwiper', {
+    spaceBetween: 30,
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+        },
+        768: {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+        }
+    }
+});
