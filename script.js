6a5ea0fcc10d04479600ba8efd659948d744348b
nastushka-15 <158353055+nastushka-15@users.noreply.github.com>
2024-09-28 23:09:22 +0300

    Revert "Vika"

--- script.js
+++ script.js
@@ -1,32 +1,3 @@
-
-// new Swiper('.card-wrapper', {
-//     loop: true,
-//     spaceBetween: 30,
-//     // Pagination bullets
-//     pagination: {
-//         el: '.swiper-pagination',
-//         clickable: true,
-//         dynamicBullets: true
-//     },
-//     // Navigation arrows
-//     navigation: {
-//         nextEl: '.swiper-button-next',
-//         prevEl: '.swiper-button-prev',
-//     },
-//     // Responsive breakpoints
-//     breakpoints: {
-//         0: {
-//             slidesPerView: 1
-//         },
-//         768: {
-//             slidesPerView: 2
-//         },
-//         1024: {
-//             slidesPerView: 3
-//         }
-//     }
-// });
-
 // process section
 const processButtons = document.querySelectorAll('.process_buttonlist__item');
 const processVideos = document.querySelectorAll('.process_video iframe');
